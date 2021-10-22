02e130ffb92f63688a46b9e49f943ecce8025990
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-22 22:36:00 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,22 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int countSheets;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCountSheets() {
+        return countSheets;
+    }
+
+    public void setCountSheets(int countSheets) {
+        this.countSheets = countSheets;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,42 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book one = new Book();
+        one.setName("CleanCode");
+        one.setCountSheets(200);
+        Book two = new Book();
+        two.setName("BadCode");
+        two.setCountSheets(100);
+        Book three = new Book();
+        three.setName("Test");
+        three.setCountSheets(50);
+        Book four = new Book();
+        four.setName("Test2");
+        four.setCountSheets(25);
+        Book[] books = new Book[4];
+        books[0] = one;
+        books[1] = two;
+        books[2] = three;
+        books[3] = four;
+        for (int index = 0; index < books.length; index++) {
+            Book currentBook = books[index];
+            System.out.println("Name: " + currentBook.getName()
+                    + ", count: " + currentBook.getCountSheets());
+        }
+        books[0] = four;
+        books[3] = one;
+        for (int index = 0; index < books.length; index++) {
+            Book currentBook = books[index];
+            System.out.println("Name: " + currentBook.getName()
+                    + ", count: " + currentBook.getCountSheets());
+        }
+        for (int index = 0; index < books.length; index++) {
+            Book currentBook = books[index];
+            if (currentBook.getName().equals("CleanCode")) {
+                System.out.println("Name: " + currentBook.getName()
+                        + ", count: " + currentBook.getCountSheets());
+            }
+        }
+    }
+}
