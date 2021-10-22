379836b82f06e3c8c31bdec55752334457137869
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-22 22:14:38 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Иванов Иван Иванович");
+        student.setGroup("Первая");
+        student.setDate(LocalDate.now());
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        System.out.println("ФИО: " + student.getFio() + "\nГруппа: " + student.getGroup() + "\nДата зачисления: " + student.getDate().format(dateTimeFormatter));
+    }
+}
