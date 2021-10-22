aef6807340f23bf823e77b097b327d8e7aa3e219
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-22 15:43:15 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{"
+                + System.lineSeparator()
+                + "\t \"name\" : " + "\"" + name + "\","
+                + System.lineSeparator()
+                + "\t \"body\" : " + "\"" + body + "\""
+                + System.lineSeparator()
+                + "}";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new JSONReport().generate("name", "body"));
+    }
+
+}
