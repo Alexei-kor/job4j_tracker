18f9140ef2814f875d07971d1be516d89d8c0d08
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-25 11:28:10 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,9 +6,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item(1, "Очень важная задача");
-        String format = "dd-MMMM-EEEE-yyyy HH:mm:ss";
-        DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern(format);
-        System.out.println(dtFormatter.format(item.getCreated()));
+        System.out.println(item);
     }
 
 }
