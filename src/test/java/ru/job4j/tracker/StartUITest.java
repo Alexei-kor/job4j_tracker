570c961b5f19b161460cba4589f1445323fc2f1a
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-28 09:45:57 +0300

    9. Написать тесты на StartUI. [#785 #220962] Отработка замечаний - удаление лишних тестов

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -9,45 +9,6 @@ import static org.hamcrest.Matchers.nullValue;
 public class StartUITest {
 
     @Test
-    public void createItem() {
-        String[] answers = new String[]{"первая", "вторая", "третья"};
-        Output output = new StubOutput();
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        new CreateItem(output).execute(input, tracker);
-        Item expected = new Item("первая");
-        Item actual = tracker.findAll()[0];
-        assertThat(actual.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenReplaceItem() {
-        Output output = new StubOutput();
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "replaced item"
-        };
-        new EditItem(output).execute(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Output output = new StubOutput();
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId())};
-        new DeleteItem(output).execute(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertNull(replaced);
-    }
-
-    @Test
     public void whenCreateItem() {
         Output output = new StubOutput();
         Input in = new StubInput(
@@ -80,12 +41,12 @@ public class StartUITest {
     }
 
     @Test
-    public void whenDeleteItem2() {
+    public void whenDeleteItem() {
         Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
-                new String[] {"0", "1", "1"}
+                new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
                 new DeleteItem(output),
