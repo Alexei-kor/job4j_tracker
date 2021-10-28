3161f11b9c27646583eb129ba0912341639e3265
Alexei_korn <aykornjushenkov@gmail.com>
2021-10-28 16:47:20 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #221128] Отработка замечаний

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -8,7 +8,7 @@ public class Fact {
     public int calc(int n) {
         int rsl = 1;
         if (n < 0) {
-            throw new IllegalArgumentException("Параметр не может быть отрицательным числом");
+            throw new IllegalArgumentException("Параметр не может быть меньше 0");
         }
         for (int index = 1; index <= n; index++) {
             rsl *= index;
