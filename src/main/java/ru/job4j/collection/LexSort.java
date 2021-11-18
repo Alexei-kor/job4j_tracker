3a28906228b526b195ed38c991038425db8552b3
Alexei_korn <aykornjushenkov@gmail.com>
2021-11-18 22:18:28 +0300

    4. Сортировка номера [#221284 #229805] Отработка замечаний

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,17 +5,8 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        left = left.replaceAll("[^0-9.]", "");
-        right = right.replaceAll("[^0-9.]", "");
         String[] arrL = left.split("\\.");
         String[] arrR = right.split("\\.");
-        for (int i = 0; i < Math.min(arrL.length, arrR.length); i++) {
-            int numL = Integer.parseInt(arrL[i]);
-            int numR = Integer.parseInt(arrR[i]);
-            if (numL != numR) {
-                return numL - numR;
-            }
-        }
-        return arrL.length - arrR.length;
+        return Integer.compare(Integer.parseInt(arrL[0]), Integer.parseInt(arrR[0]));
     }
 }
