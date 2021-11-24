0b7041b70181c052250d413e1185b73361e717cb
Alexei_korn <aykornjushenkov@gmail.com>
2021-11-24 23:07:25 +0300

    2.2. Лямбда блок [#249208] замечания CI Bot

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -11,7 +11,8 @@ public class LambdaUsage {
         Comparator<String> comparator = (o1, o2) -> {
             System.out.println("Строка 1: " + o1 + System.lineSeparator() + "Строка 2: " + o2);
             int rsl = o2.compareTo(o1);
-            System.out.println(rsl < 0 ? "Строка 1 больше Строки 2" : rsl == 0 ? "Строки равны" : "Строка 1 меньше строки 2");
+            System.out.println(rsl < 0 ? "Строка 1 больше Строки 2"
+                    : rsl == 0 ? "Строки равны" : "Строка 1 меньше строки 2");
             return rsl;
         };
         list.sort(comparator);
