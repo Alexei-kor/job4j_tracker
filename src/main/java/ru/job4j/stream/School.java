29e8b67cfab8dfabef9b888e4ca267466dfcd06e
Alexei_korn <aykornjushenkov@gmail.com>
2021-11-28 15:01:12 +0300

    1. Фильтрация учеников [#110058 #233471] Отработка замечаний

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * @author Alexei
+ */
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
