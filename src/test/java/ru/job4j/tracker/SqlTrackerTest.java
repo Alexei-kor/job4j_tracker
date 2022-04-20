9111c1ed0a1e38c692e8068a9c4460c76c23534d
Alexei_korn <aykornjushenkov@gmail.com>
2022-04-20 21:13:53 +0300

    3. Liquibase. Интеграционные тесты. [#95881] Отработка замечаний

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -54,50 +54,44 @@ public class SqlTrackerTest {
     @Test
     public void whenSaveItemAndFindByNameThenMustBeTheSame() throws SQLException {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item2");
-        tracker.add(item);
-        List<Item> expected = Arrays.asList(item);
-        assertThat(tracker.findByName(item.getName()), is(expected));
+        Item item1 = tracker.add(new Item("item1"));
+        Item item2 = tracker.add(new Item("item2"));
+        Item item3 = tracker.add(new Item("item3"));
+        Item item11 = tracker.add(new Item("item1"));
+        assertThat(tracker.findByName("item1"), is(List.of(item1, item11)));
     }
 
     @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() throws SQLException {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item2");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item2"));
         assertThat(tracker.findById(item.getId()), is(item));
     }
 
     @Test
     public void whenSaveItemAndReplaceNameAndFindByNewNameThenMustBeTheSame() throws SQLException {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         item.setName("ItemNew");
         tracker.replace(item.getId(), item);
-        List<Item> expected = Arrays.asList(item);
-        assertThat(tracker.findByName(item.getName()), is(expected));
+        assertThat(tracker.findById(item.getId()).getName(), is(item.getName()));
     }
 
     @Test
     public void whenSave2ItemAndSelectAllThenMustBe2Items() throws SQLException {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item1");
-        Item item2 = new Item("item2");
-        tracker.add(item1);
-        tracker.add(item2);
+        Item item1 = tracker.add(new Item("item1"));
+        Item item2 = tracker.add(new Item("item2"));
         assertThat(tracker.findAll().size(), is(2));
     }
 
     @Test
     public void whenSaveTwoItemAndDeleteOneThenMustBeOneSame() throws SQLException {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = new Item("item1");
-        Item item2 = new Item("item2");
-        tracker.add(item1);
-        tracker.add(item2);
+        Item item1 = tracker.add(new Item("item1"));
+        Item item2 = tracker.add(new Item("item2"));
         tracker.delete(item1.getId());
-        assertThat(tracker.findById(item2.getId()), is(item2));
+        assertNull(tracker.findById(item1.getId()));
     }
 
 }
